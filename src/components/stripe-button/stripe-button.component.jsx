10359edb52aae34d37c0be8c3829ee37450363cf
Outlander <sujilgovind@users.noreply.github.com>
2021-09-27 11:54:29 +0530

    added toast message for successful payment

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import StripeCheckout from 'react-stripe-checkout';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishabeKey = 'pk_test_51JdAvNSHQTvmiO5K9XjriaofZW89YOvFfMBNBgdIDay7Sr7voXqAHAbwMIHYQ8sPHIF9aTtECoEoH6Ir1k2VGWwN009QsJDUxq';
         const onToken = token => {
             console.log(token);
-            alert('Payment Successfull');
+            //alert('Payment Successfull');
+            toast.configure();
+            toast.success('Payment Successfull', {position: toast.POSITION.TOP_RIGHT});
         }
 
     return (
